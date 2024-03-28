98a013f0a669fbd801d86f937ef15f0e84f71bd7
kaiqueoliveiramendes <129184576+kaiqueoliveiramendes@users.noreply.github.com>
2024-03-28 09:08:49 -0300

    p

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 let tempoAtual = new Date();
 
+
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
